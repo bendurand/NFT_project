ddb56fd1e0acae42b7f3299b71f319e6bf8c1e66
bendurand <ben.durand20@gmail.com>
2022-12-01 22:22:09 +0100

    commit

--- scripts/mint.js
+++ scripts/mint.js
@@ -9,9 +9,9 @@ const hre = require("hardhat");
 async function main() {
 
   const TiketEvent = await hre.ethers.getContractFactory("tiketEvent");
-  const tiketEvent = await TiketEvent.attach("0x47eE3c385b577a48e630F9265C0c85c69Ba4FBC1");
+  const tiketEvent = await TiketEvent.attach("0x47ee3c385b577a48e630f9265c0c85c69ba4fbc1");
 
-  await tiketEvent.mintNFT("0xEbD05b2e709D17857E0e12e2067D15900064dc90", "https://gateway.pinata.cloud/ipfs/QmQKGtWEgGXRNFH3ptbUJi2uGqpQ2ADva3RHtKacHqYE9E");
+  await tiketEvent.mintNFT("0xEbD05b2e709D17857E0e12e2067D15900064dc90", "https://gateway.pinata.cloud/ipfs/QmePapaXEVfiNwxYeRh2dy7dx6wQKcAhu2yVfRLYemgrNH/NFT_france_allblacks_vert.png");
 
   console.log(
     ` deployed to ${tiketEvent.address}`
