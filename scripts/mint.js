da90a071be23f26eb1017c589e2348ae9c078e76
bendurand <ben.durand20@gmail.com>
2022-12-02 00:42:20 +0100

    Final commit

--- scripts/mint.js
+++ scripts/mint.js
@@ -9,9 +9,9 @@ const hre = require("hardhat");
 async function main() {
 
   const TiketEvent = await hre.ethers.getContractFactory("tiketEvent");
-  const tiketEvent = await TiketEvent.attach("0x47eE3c385b577a48e630F9265C0c85c69Ba4FBC1");
+  const tiketEvent = await TiketEvent.attach("0xBEb96213BC62971206795341E7cEC3310a9f1A3E");
 
-  await tiketEvent.mintNFT("0xEbD05b2e709D17857E0e12e2067D15900064dc90", "https://gateway.pinata.cloud/ipfs/QmQKGtWEgGXRNFH3ptbUJi2uGqpQ2ADva3RHtKacHqYE9E");
+  await tiketEvent.mintNFT("0xEbD05b2e709D17857E0e12e2067D15900064dc90", "QmZ5dbhXs9piBkuxtJyBbr6BfVbkDo8mhuRqv8hWmmVGmv");
 
   console.log(
     ` deployed to ${tiketEvent.address}`
