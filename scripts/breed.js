b76d01975e305bcdb25b085cea4adcb5beb292a0
bendurand <ben.durand20@gmail.com>
2022-12-02 01:48:48 +0100

    commit

--- scripts/breed.js
+++ scripts/breed.js
@@ -8,6 +8,10 @@ const hre = require("hardhat");
 
 async function main() {
 
+  const _smart_contract_address = "0xBEb96213BC62971206795341E7cEC3310a9f1A3E"
+
+  const _user_address = "0xEbD05b2e709D17857E0e12e2067D15900064dc90"
+
   const _category1 = "QmZ5dbhXs9piBkuxtJyBbr6BfVbkDo8mhuRqv8hWmmVGmv"
   const _category2 = "QmVqcD5DZx9yrY6f6ZJ1xTXksk8YsR2HutKrpy481xciQK"
   const _category3 = "QmXv1LAfD5qfg7yZQ6RHtAYb1DhSKJ5MSHzKs6SP4dSPkp"
@@ -15,9 +19,9 @@ async function main() {
   const _category5 = "QmXDgpBFmq9qNao1HQsWCmaUXTp1Pr2MwL6FgohpewdsvT"
 
   const TiketEvent = await hre.ethers.getContractFactory("tiketEvent");
-  const tiketEvent = await TiketEvent.attach("0xBEb96213BC62971206795341E7cEC3310a9f1A3E");
+  const tiketEvent = await TiketEvent.attach(_smart_contract_address);
 
-  await tiketEvent.fusion(9,10,"0xEbD05b2e709D17857E0e12e2067D15900064dc90", _category3);
+  await tiketEvent.fusion(9,10,_user_address, _category3);
 
   console.log(
     ` breeded and deployed to ${tiketEvent.address}`
